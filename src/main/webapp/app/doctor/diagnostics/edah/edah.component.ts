fdb88720f6a657911cc7b9dd665a31e756c25566
CARLOS MOISES CAMPOVERDE HERRERA <cmcampoverde@uce.edu.ec>
2021-08-28 11:30:12 -0500

    clean of Ts-Ignore

--- src/main/webapp/app/doctor/diagnostics/edah/edah.component.ts
+++ src/main/webapp/app/doctor/diagnostics/edah/edah.component.ts
@@ -11,30 +11,6 @@ import { PatientService } from '../../../entities/patient/patient.service';
   styleUrls: ['./edah.component.scss'],
 })
 export class EdahComponent implements OnInit {
-  /* dataSource = [
-    { question: 'Tiene excesiva inquietud motora', type: 'H' },
-    { question: 'Tiene dificultades de aprendizaje escolar', type: 'DA' },
-    { question: 'Molesta frecuentemente a otros niños', type: 'H' },
-    { question: 'Se distrae fácilmente, muestra escasa atención', type: 'DA' },
-    { question: 'Exige inmediata satisfacción a sus demandas', type: 'H' },
-    { question: 'Tiene dificultad para las actividades cooperativa', type: 'TC' },
-    { question: 'Está en las nubes, ensimismado', type: 'DA' },
-    { question: 'Deja por terminar las tareas que empieza', type: 'DA' },
-    { question: 'Es mal aceptado por el grupo', type: 'TC' },
-    { question: 'Niega sus errores o echa la culpa a otros', type: 'TC' },
-    { question: 'A menudo grita en situaciones inadecuadas', type: 'TC' },
-    { question: 'Contesta con facilidad. Es irrespetuoso y arrogante', type: 'TC' },
-    { question: 'Se mueve constantemente, intranquilo', type: 'H' },
-    { question: 'Discute y pelea por cualquier cosa', type: 'TC' },
-    { question: 'Tiene explosiones impredecibles de mal genio', type: 'TC' },
-    { question: 'Le falta sentido de la regla, del “juego limpio', type: 'TC' },
-    { question: 'Es impulsivo e irritable', type: 'H' },
-    { question: 'Se lleva mal con la mayoría de sus compañeros', type: '  TC' },
-    { question: 'Sus esfuerzos se frustran fácilmente, es inconstante', type: '  DA' },
-    { question: 'Acepta mal las críticas del profeso', type: '  TC' },
-  ];
-   */
-
   displayedColumns: string[] = ['question', 'options', 'type'];
 
   dataSource?: IQuestion[];
@@ -53,9 +29,7 @@ export class EdahComponent implements OnInit {
   }
 
   loadEdah = () => {
-    this.questionService.query().subscribe(response => {
-      // eslint-disable-next-line no-console
-      console.log(response);
+    this.questionService.query().subscribe((response: { body: IQuestion[]; }) => {
       this.dataSource = response.body as IQuestion[];
     });
   };
@@ -65,9 +39,9 @@ export class EdahComponent implements OnInit {
     testEdah.teacherName = this.teacherName;
     testEdah.teacherEmail = this.teacherEmail;
     testEdah.instructions = this.edahDetails;
-    // @ts-ignore
-    testEdah.patientId = this.patientService.currentPatient.id;
-    this.testEdahService.sendEmailEdah(testEdah).subscribe(response => {
+    const patientServiceAux = this.patientService.currentPatient;
+    testEdah.patientId = patientServiceAux ? patientServiceAux.id : 0;
+    this.testEdahService.sendEmailEdah(testEdah).subscribe((response: any) => {
       // eslint-disable-next-line no-console
       console.log(response);
     });
